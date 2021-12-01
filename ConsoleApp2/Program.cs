57b83b754545c7d36c207a8aedeb5d778cdc374a
narmada1999 <narmada.l@somaiya.edu>
2021-12-01 18:09:03 +0530

    pull repo

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Exploring Git");
+        }
+    }
+}
